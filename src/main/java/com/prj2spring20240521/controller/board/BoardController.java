16226d4a50c51aeef6fa619d99b0ec604fd5f03d
Jin Jin <anj72005@gmail.com>
2024-05-21 15:33:10 +0900

    게시물 삭제

--- src/main/java/com/prj2spring20240521/controller/board/BoardController.java
+++ src/main/java/com/prj2spring20240521/controller/board/BoardController.java
@@ -38,4 +38,9 @@ public class BoardController {
         }
         return ResponseEntity.ok().body(board);
     }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id) {
+        service.remove(id);
+    }
 }
