e72b70a87c843d7a843487e4d20e9bc60580325a
Jin Jin <anj72005@gmail.com>
2024-05-22 15:11:53 +0900

    회원 목록 보기

--- src/main/java/com/prj2spring20240521/service/member/MemberService.java
+++ src/main/java/com/prj2spring20240521/service/member/MemberService.java
@@ -7,6 +7,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(rollbackFor = Exception.class)
 @RequiredArgsConstructor
@@ -44,5 +46,9 @@ public class MemberService {
         }
 
         return true;
+    }
+
+    public List<Member> getAll() {
+        return mapper.selectAll();
     }
 }
