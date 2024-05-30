3cac1920ec3133970afff6a4e1cd993db3aaadf7
Jin Jin <anj72005@gmail.com>
2024-05-30 11:19:46 +0900

    댓글 작성하면 comment table에 저장

--- sql/project.sql
+++ sql/project.sql
@@ -102,3 +102,15 @@ CREATE TABLE board_like
     member_id INT NOT NULL REFERENCES member (id),
     PRIMARY KEY (board_id, member_id)
 );
+
+CREATE TABLE comment
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    board_id  INT          NOT NULL REFERENCES board (id),
+    member_id INT          NOT NULL REFERENCES member (id),
+    comment   VARCHAR(500) NOT NULL,
+    inserted  DATETIME     NOT NULL DEFAULT NOW()
+);
+
+SELECT *
+FROM comment;
--- src/main/java/com/prj2spring20240521/controller/comment/CommentController.java
+++ src/main/java/com/prj2spring20240521/controller/comment/CommentController.java
@@ -1,7 +1,9 @@
 package com.prj2spring20240521.controller.comment;
 
 import com.prj2spring20240521.domain.comment.Comment;
+import com.prj2spring20240521.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
 public class CommentController {
+    final CommentService service;
 
     @PostMapping("add")
-    public void addComment(@RequestBody Comment comment) {
-        System.out.println("comment = " + comment);
+    public void addComment(@RequestBody Comment comment, Authentication authentication) {
+        service.add(comment, authentication);
     }
 }
